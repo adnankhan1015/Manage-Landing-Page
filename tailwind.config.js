ca77a8be479bfeb06686540f52a97f718554a477
adnankhan <adnankhannad@gmail.com>
2025-02-15 21:58:00 +0500

    add changes

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,23 +1,28 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./*.html'],
+  content: [
+    "./public/**/*.html", // Include all HTML files inside the public/ folder
+    "./*.html", // Include root-level HTML files
+    "./public/js/**/*.js", // Include all JS files inside public/js/
+    "./src/**/*.{html,js}", // Include any source files (if used)
+  ],
   theme: {
     screens: {
-      sm: '480px',
-      md: '768px',
-      lg: '976px',
-      xl: '1440px',
+      sm: "480px",
+      md: "768px",
+      lg: "976px",
+      xl: "1440px",
     },
     extend: {
       colors: {
-        brightRed: 'hsl(12, 88%, 59%)',
-        brightRedLight: 'hsl(12, 88%, 69%)',
-        brightRedSupLight: 'hsl(12, 88%, 95%)',
-        darkBlue: 'hsl(228, 39%, 23%)',
-        darkGrayishBlue: 'hsl(227, 12%, 61%)',
-        veryDarkBlue: 'hsl(233, 12%, 13%)',
-        veryPaleRed: 'hsl(13, 100%, 96%)',
-        veryLightGray: 'hsl(0, 0%, 98%)',
+        brightRed: "hsl(12, 88%, 59%)",
+        brightRedLight: "hsl(12, 88%, 69%)",
+        brightRedSupLight: "hsl(12, 88%, 95%)",
+        darkBlue: "hsl(228, 39%, 23%)",
+        darkGrayishBlue: "hsl(227, 12%, 61%)",
+        veryDarkBlue: "hsl(233, 12%, 13%)",
+        veryPaleRed: "hsl(13, 100%, 96%)",
+        veryLightGray: "hsl(0, 0%, 98%)",
       },
     },
   },
